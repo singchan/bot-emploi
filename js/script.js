d7bb5bc81f9689a65e50258fe802ed0e431e2a11
singchan <billy321@im-paris.fr>
2017-04-03 23:05:56 +0200

    script to resize the form (#25)

--- js/script.js
+++ js/script.js
@@ -2,4 +2,13 @@
         if (location.protocol != 'https:' && location.href.indexOf('silex.me') === -1) {
             location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
         }
+        
+        $(function() {
+            function resize() {
+                $('.full-height').css('min-height', $(window).height());
+            }
+            resize();
+            $(window).on('resize', resize);
+        //    $(document.body).on('silex:resize', resize);
+        })
     
